769e685dacd1160bbb887f46c2363406ad180fc1
vvelasquezs <virginiavelasquez16@gmail.com>
2015-04-06 14:31:24 -0500

    Solucionar header

--- js/script.js
+++ js/script.js
@@ -715,3 +715,46 @@ function recallMagnific(){
 function Resize() {
 	$(window).trigger('resize');
 }
+
+
+/* ---------------------------------------------------------------------- */
+/*	Header-top animate remove after scroll
+/* ---------------------------------------------------------------------- */
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.header-top').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.header-top').removeClass('header-down').addClass('header-up');
+        $('.navbar').addClass('navbar-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.header-top').removeClass('header-up').addClass('header-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
